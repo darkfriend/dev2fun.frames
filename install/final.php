556783ab420f343a6f545c54678bf9a084408f11
darkfriend <maslov.tlt@gmail.com>
2025-11-10 02:34:21 +0300

    fix final

--- install/final.php
+++ install/final.php
@@ -1,6 +1,24 @@
 <?php
+/**
+ * @author darkfriend <hi@darkfriend.ru>
+ * @copyright dev2fun
+ * @version 1.0.0
+ */
 defined('B_PROLOG_INCLUDED') and (B_PROLOG_INCLUDED === true) or die();
+if(!check_bitrix_sessid()) return;
 
-$MESS["DEV2FUN_FRAMES_INSTALL_SUCCESS"] = 'Модуль успешно установлен';
-$MESS["DEV2FUN_FRAMES_INSTALL_LAST_MSG"] = "<a href='/bitrix/admin/settings.php?mid=dev2fun.frames&lang=ru'><b>Перейти к настройкам модуля</b></a><br/><br/>
-<b>Будем благодарны предложениям по улучшению модуля</b><br>Если вам необходима <b>команда разработки</b> обращайтесь на почту: <a href='mailto:support@dev2fun.com'>support@dev2fun.com</a>";
+use Bitrix\Main\Loader,
+    Bitrix\Main\Localization\Loc;
+
+IncludeModuleLangFile(__FILE__);
+
+Loader::includeModule('main');
+
+CAdminMessage::ShowMessage(array(
+    "MESSAGE"=>Loc::getMessage('D2F_IFRAMELAZY_INSTALL_SUCCESS'),
+    "TYPE"=>"OK"
+));
+
+echo BeginNote();
+echo Loc::getMessage("D2F_IFRAMELAZY_INSTALL_LAST_MSG");
+echo EndNote();
